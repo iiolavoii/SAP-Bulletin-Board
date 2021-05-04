385bf4db5124485a020f412d696d0ee5f6568d35
OlavoGomes <iiolavoii@gmail.com>
2021-05-04 10:13:14 +0100

    Step 5 Added flag button

--- sap.m.tutorial.testing.01/webapp/model/FlaggedType.js
+++ sap.m.tutorial.testing.01/webapp/model/FlaggedType.js
@@ -3,14 +3,17 @@ sap.ui.define([
 ], function(SimpleType) {
     'use strict';
     return SimpleType.extend("sap.ui.demo.bulletinboard.model.FlaggedType", {
-        formatValue:function(){
-
+        formatValue:function(iFlagged){
+            return iFlagged === 1;
         },
-        parseValue: function(){
-
+        parseValue: function(bFlagged){
+            if(bFlagged){
+                return 1;
+            }
+            return 0;
         },
         validateValue: function(){
-
+            return true;
         }
     });
     
